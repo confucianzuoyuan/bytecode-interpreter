1ae036e7441df803a3241889843d5a692caf84fc
confucianzuoyuan <zuoyuantc@126.com>
2024-04-25 17:21:41 +0800

    update

--- interpreter.ml
+++ interpreter.ml
@@ -7,8 +7,10 @@ type stackValue =
   | String of string
   | Name of string
   | Unit
- 
-type command =
+  | FunDef of string * string * command list
+  | InOutFunDef of string * string * command list
+
+and command =
   | Add
   | Sub
   | Mul
@@ -31,6 +33,11 @@ type command =
   | End
   | ToString
   | Println
+  | Fun of string * string
+  | InOutFun of string * string
+  | FunEnd
+  | Return
+  | Call
 
 let is_integer s = try let _ = int_of_string s in true with _ -> false
 
@@ -77,8 +84,10 @@ let stackvalue_to_string = function
   | Unit -> ":unit:"
   | String s -> s
   | Integer i -> string_of_int i
-  | Name name -> name
+  | Name name -> "Name: " ^ name
   | Error -> ":error:"
+  | FunDef (name, _, _) -> "FunDef " ^ name
+  | InOutFunDef (name, _, _) -> "InOutFunDef " ^ name 
 
 let command_to_string = function
   | Add -> "add"
@@ -103,6 +112,19 @@ let command_to_string = function
   | End -> "end"
   | ToString -> "toString"
   | Println -> "println"
+  | Fun (name, arg) -> "fun " ^ name ^ " " ^ arg
+  | InOutFun (name, arg) -> "inOutFun " ^ name ^ " " ^ arg
+  | FunEnd -> "funEnd"
+  | Return -> "return"
+  | Call -> "call"
+
+let rec print_command_list = function
+  | [] -> ()
+  | hd :: tl -> let _ = print_string ((command_to_string hd) ^ "\n") in print_command_list tl
+
+let rec print_stackvalue_list = function
+  | [] -> ()
+  | hd :: tl -> let _ = print_string ("res: " ^ (stackvalue_to_string hd) ^ "\n") in print_stackvalue_list tl
 
 let string_to_command = function
   | "add" -> Add
@@ -126,9 +148,14 @@ let string_to_command = function
   | "end" -> End
   | "toString" -> ToString
   | "println" -> Println
+  | "funEnd" -> FunEnd
+  | "return" -> Return
+  | "call" -> Call
   | cmd ->
     let cmd_list =  String.split_on_char ' ' cmd in
     match cmd_list with
+    | ["fun"; funname; arg] -> Fun (funname, arg)
+    | ["inOutFun"; funname; arg] -> InOutFun (funname, arg)
     | "push" :: tl -> Push (string_to_stackvalue (String.sub cmd 5 (String.length cmd - 5)))
     | _ -> failwith ("no command: " ^ cmd)
 
@@ -136,8 +163,6 @@ let rec get env name =
    match env with
    | [] -> Error
    | hd :: tl -> if String.compare (fst hd) name == 0 then snd hd else get tl name
-
-
 
 let rec print_env env =
    match env with
@@ -192,6 +217,22 @@ let cat a b env =
 let sub a b env =
    match a, b with
    | Integer i, Integer j -> [Integer (j - i)]
+   | Name name, Integer j ->
+      (match get env name with
+       | Error -> [Error; a; b]
+       | Integer i -> [Integer (j - i)]
+       | _ -> [Error; a; b])
+   | Integer i, Name name ->
+      (match get env name with
+       | Error -> [Error; a; b]
+       | Integer j -> [Integer (j - i)]
+       | _ -> [Error; a; b])
+   | Name n1, Name n2 ->
+      (match get env n1, get env n2 with
+       | Error, _ -> [Error; a; b]
+       | _, Error -> [Error; a; b]
+       | Integer i, Integer j -> [Integer (j - i)]
+       | _ -> [Error; a; b])
    | _ -> [Error; a; b]
 
 let mul a b env =
@@ -201,6 +242,11 @@ let mul a b env =
       (match get env name with
        | Error -> [Error; a; b]
        | Integer i -> [Integer (i * j)]
+       | _ -> [Error; a; b])
+   | Integer i, Name name ->
+      (match get env name with
+       | Error -> [Error; a; b]
+       | Integer j -> [Integer (i * j)]
        | _ -> [Error; a; b])
    | Name n1, Name n2 ->
       (match get env n1, get env n2 with
@@ -277,9 +323,25 @@ let op_not a env =
    | Bool false -> [Bool true]
    | _ -> [Error; a]
 
-let equal a b env =
+let op_equal a b env =
    match a, b with
    | Integer i, Integer j -> if i == j then [Bool true] else [Bool false]
+   | Name name, Integer j ->
+      (match get env name with
+       | Error -> [Error; a; b]
+       | Integer i -> if i == j then [Bool true] else [Bool false]
+       | _ -> [Error; a; b])
+   | Integer i, Name name ->
+      (match get env name with
+       | Error -> [Error; a; b]
+       | Integer j -> if i == j then [Bool true] else [Bool false]
+       | _ -> [Error; a; b])
+   | Name n1, Name n2 ->
+      (match get env n1, get env n2 with
+       | Error, _ -> [Error; a; b]
+       | _, Error -> [Error; a; b]
+       | Integer i, Integer j -> if i == j then [Bool true] else [Bool false]
+       | _ -> [Error; a; b])
    | _ -> [Error; a; b]
 
 let less_than a b env =
@@ -321,8 +383,12 @@ let cmd_if a b c env =
    | Bool false -> [b]
    | _ -> [Error; a; b; c]
 
+let rec parse_fundef cmds acc =
+   match Stream.next cmds with
+   | FunEnd -> acc
+   | cmd -> cmd :: parse_fundef cmds acc
+
 let rec execute commands stack env oc =
-   let _ = print_env env in 
    match Stream.next commands with
    (* | [] -> stack *)
    | (Push stack_value) -> execute commands (stack_value :: stack) env oc
@@ -382,7 +448,7 @@ let rec execute commands stack env oc =
       (match stack with
        | [] -> execute commands [Error] env oc
        | [v] -> execute commands [Error; v] env oc
-       | a :: b :: stl -> execute commands ((equal a b env) @ stl) env oc)
+       | a :: b :: stl -> execute commands ((op_equal a b env) @ stl) env oc)
    | LessThan ->
       (match stack with
        | [] -> execute commands [Error] env oc
@@ -421,7 +487,44 @@ let rec execute commands stack env oc =
       (match stack with
        | [] -> [Error]
        | shd :: _ -> [shd])
+   | Fun (name, arg) ->
+      let block = parse_fundef commands [] in
+      let _ = print_command_list block in
+      let new_env = (name, FunDef (name, arg, block)) :: env in
+      execute commands (Unit :: stack) new_env oc
+   | InOutFun (name, arg) ->
+      let block = parse_fundef commands [] in
+      let new_env = (name, InOutFunDef (name, arg, block)) :: env in
+      execute commands (Unit :: stack) new_env oc
+   | Call ->
+      (match stack with
+       | [] -> execute commands [Error] env oc
+       | [v] -> execute commands [Error; v] env oc
+       | param :: (Name name) :: tl ->
+         let (res, new_env) = execute_funcall name param env oc in
+         execute commands (res :: tl) new_env oc
+       | _ -> execute commands (Error :: stack) env oc)
+   | Return ->
+      (match stack with
+       | [] -> [Error]
+       | hd::_ -> [hd])
    | _ -> [Error]
+
+and execute_funcall funname param env oc =
+   match get env funname with
+   | FunDef (name, arg, body) ->
+      let new_env = (arg, param) :: env in
+      let res = execute (Stream.of_list body) [] new_env oc in
+      (match res with
+      | [v] -> (v, env)
+      | _ -> (Error, env))
+   | InOutFunDef (name, arg, body) ->
+      let new_env = (arg, param) :: env in
+      let res = execute (Stream.of_list body) [] new_env oc in
+      (match res, param with
+      | [v], Name new_arg -> (v, (new_arg, get new_env arg) :: env)
+      | _ -> (Error, env))
+   | _ -> (Error, env)
 
 let interpreter (input, output) =
   let ic = open_in input
